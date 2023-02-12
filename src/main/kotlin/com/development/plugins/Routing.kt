7887241e21eb5527763d8ec51e6f7ff74d91b72a
Risto12 <risto.vesanto1112@gmail.com>
2023-02-12 20:30:04 +0200

    feat: socket endpoint for chat program

--- src/main/kotlin/com/development/plugins/Routing.kt
+++ src/main/kotlin/com/development/plugins/Routing.kt
@@ -1,13 +1,15 @@
 package com.development.plugins
 
 import com.development.routing.alienNews
-import com.development.routing.chat
+import com.development.routing.chatSocket
+import com.development.routing.echoSocket
 import io.ktor.server.routing.routing
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     routing {
         alienNews()
-        chat()
+        echoSocket()
+        chatSocket()
     }
 }
--- src/main/kotlin/com/development/routing/Chat.kt
+++ src/main/kotlin/com/development/routing/Chat.kt
@@ -1,28 +1,54 @@
 package com.development.routing
 
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
+import com.development.components.Connection
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import java.util.*
+import kotlin.collections.LinkedHashSet
 
-const val CHAT_ROOT_PATH = "/chat"
+const val SOCKET_ROOT_PATH = "/socket"
+const val CHAT_PATH = "$SOCKET_ROOT_PATH/chat"
+const val ECHO_PATH = "$SOCKET_ROOT_PATH/echo"
 
 /***
- * Socket based route
+ * Echo socket for testing
  */
-fun Route.chat() {
-    webSocket(CHAT_ROOT_PATH) {
-        send("Please enter your name")
+fun Route.echoSocket() {
+    webSocket(ECHO_PATH) {
+        send("Connection established")
         for (frame in incoming) {
             frame as? Frame.Text ?: continue
             val receivedText = frame.readText()
-            if (receivedText.equals("bye", ignoreCase = true)) {
-                close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+            if (receivedText.equals("quit", ignoreCase = true)) {
+                close(CloseReason(CloseReason.Codes.NORMAL, "Connection ended"))
             } else {
-                send(Frame.Text("Hi, $receivedText!"))
+                send(Frame.Text("Echo, $receivedText!"))
             }
+        }
+    }
+}
+
+fun Route.chatSocket() {
+    val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
+    webSocket(CHAT_PATH) {
+        val thisConnection = Connection(this)
+        connections += thisConnection
+        try {
+            send("You are connected! There are ${connections.count()} users here.")
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                val textWithUsername = "[${thisConnection.name}]: $receivedText"
+
+                connections.forEach {
+                    it.session.send(textWithUsername)
+                }
+            }
+        } catch (e: Exception) {
+            println(e.localizedMessage)
+        } finally {
+            connections -= thisConnection
         }
     }
 }
